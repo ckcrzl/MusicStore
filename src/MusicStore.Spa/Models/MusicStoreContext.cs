6eb2e5eda310c51b659007bab3a1803fa435da2b
DamianEdwards <damian@damianedwards.com>
2014-05-09 23:04:52 -0700

    Home page of MusicStore.Spa working

---
+++ src/MusicStore.Spa/Models/MusicStoreContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.InMemory;
+using Microsoft.Data.Entity.SqlServer;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MusicStoreContext(IServiceProvider serviceProvider, IConfiguration configuration)
+            : base(serviceProvider)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptions builder)
+        {
+#if NET45
+            builder.UseSqlServer(_configuration.Get("Data:DefaultConnection:ConnectionString"));
+#else
+            builder.UseInMemoryStore(persist: true);
+#endif
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Album>().ToTable("Albums");
+            builder.Entity<Artist>().ToTable("Artists");
+            builder.Entity<Order>().ToTable("Orders");
+            builder.Entity<Genre>().ToTable("Genres");
+            builder.Entity<CartItem>().ToTable("CartItems");
+            builder.Entity<OrderDetail>().ToTable("OrderDetails");
+
+            builder.Entity<Album>().Key(a => a.AlbumId);
+            builder.Entity<Artist>().Key(a => a.ArtistId);
+            builder.Entity<Order>().Key(o => o.OrderId).StorageName("[Order]");
+            builder.Entity<Genre>().Key(g => g.GenreId);
+            builder.Entity<CartItem>().Key(ci => ci.CartItemId);
+            builder.Entity<OrderDetail>().Key(od => od.OrderDetailId);
+
+            builder.Entity<Album>()
+                .ForeignKeys(kb =>
+                {
+                    kb.ForeignKey<Genre>(a => a.GenreId);
+                    kb.ForeignKey<Artist>(a => a.ArtistId);
+                });
+            builder.Entity<OrderDetail>()
+                .ForeignKeys(kb =>
+                {
+                    kb.ForeignKey<Album>(a => a.AlbumId);
+                    kb.ForeignKey<Order>(a => a.OrderId);
+                });
+
+            var genre = builder.Model.GetEntityType(typeof(Genre));
+            var album = builder.Model.GetEntityType(typeof(Album));
+            var artist = builder.Model.GetEntityType(typeof(Artist));
+            var orderDetail = builder.Model.GetEntityType(typeof(OrderDetail));
+            genre.AddNavigation(new Navigation(album.ForeignKeys.Single(k => k.ReferencedEntityType == genre), "Albums"));
+            album.AddNavigation(new Navigation(orderDetail.ForeignKeys.Single(k => k.ReferencedEntityType == album), "OrderDetails"));
+        }
+    }
+}
---
+++ src/MusicStore.Spa/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.FileSystems;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+using MusicStore.Models;
+
+namespace MusicStore.Spa
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions { FileSystem = new PhysicalFileSystem("public") });
+
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+                services.AddEntityFramework()
+                    .AddSqlServer()
+                    .AddInMemoryStore();
+                services.AddScoped<MusicStoreContext>();
+            });
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(null, "api/genres/menu", new { controller = "GenresApi", action = "GenreMenuList" });
+                routes.MapRoute(null, "api/albums/mostPopular", new { controller = "AlbumsApi", action = "MostPopular" });
+                routes.MapRoute(null, "api/albums/all", new { controller = "AlbumsApi", action = "All" });
+                routes.MapRoute(null, "api/albums/{albumId}", new { controller = "AlbumsApi", action = "Details" });
+                routes.MapRoute(null, "api/genres/{genreId}/albums", new { controller = "GenresApi", action = "GenreAlbums" });
+                routes.MapRoute(null, "{controller}/{action}", new { controller = "Home", action = "Index" });
+            });
+
+            SampleData.InitializeMusicStoreDatabaseAsync(app.ApplicationServices).Wait();
+        }
+    }
+}
